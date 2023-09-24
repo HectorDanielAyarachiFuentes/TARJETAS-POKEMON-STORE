8d909797dbeaa9453a62cba88435e110a1653450
Ayarachi Fuentes Hector Daniel <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-09-24 00:20:30 -0300

    borrando espacios

--- script-detalle-pokemon.js
+++ script-detalle-pokemon.js
@@ -2,13 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
     // Obtén el ID del Pokémon desde la URL
     const urlParams = new URLSearchParams(window.location.search);
     const id = urlParams.get("id");
-
     // Obtén los datos de Pokémon desde el localStorage
     const pokemonData = JSON.parse(localStorage.getItem("pokemonData"));
-
     // Accede al Pokémon seleccionado por su ID
     const selectedPokemon = pokemonData.data[id];
-
     // Crea el contenido HTML con la información detallada del Pokémon
     const detalleHtml = `
         <div class="pokemon-card">
@@ -41,44 +38,34 @@ document.addEventListener("DOMContentLoaded", function () {
           </div>
           <button class="buy-button" onclick="mostrarMensaje()">Votar</button>
           <a href="index.html" class="back-button">Volver</a>
-
         </div>
     `;
-
     // Muestra los detalles del Pokémon seleccionado en el contenedor correspondiente
     const pokemonDetails = document.getElementById("pokemon-details");
     pokemonDetails.innerHTML = detalleHtml;
 });
-
 function realizarCompra() {
     // Obtener la cantidad y el precio
     const cantidad = parseFloat(document.getElementById('quantity').value);
     const precioUnitario = parseFloat(document.getElementById('total-price').textContent.replace('$', ''));
-
     // Calcular el precio total
     const precioTotal = cantidad * precioUnitario;
-
     // Dar formato al precio total con dos decimales
     const precioTotalFormateado = precioTotal.toFixed(2);
-
     // Mostrar una alerta de confirmación
     const confirmacion = window.confirm(`El precio total es: $${precioTotalFormateado}. ¿Deseas realizar la compra?`);
-
     if (confirmacion) {
         // Cambiar el texto del botón
         const botonCompra = document.querySelector('.buy-button');
         botonCompra.textContent = 'Producto Enviado';
-
         // Simular el envío del producto (aquí puedes agregar lógica adicional si es necesario)
         setTimeout(() => {
             window.alert('Producto enviado. ¡Gracias por tu compra!');
         }, 2000); // Simulamos el envío después de 2 segundos
-
         // Deshabilitar el botón después de la compra
         botonCompra.disabled = true;
     }
 }
-
 function mostrarMensaje() {
     alert("Gracias por votar");
 }
--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     // URL del JSON (reemplaza con la ubicación real del JSON)
     const jsonURL = "cards-pokemon-1.json";
-
     // Elemento donde se mostrarán los Pokémon
     const pokemonContainer = document.getElementById("pokemon-container");
-
     // Realiza una solicitud para cargar el JSON
     fetch(jsonURL)
         .then((response) => response.json())
         .then((data) => {
             // Almacena los datos del JSON en localStorage
             localStorage.setItem("pokemonData", JSON.stringify(data));
-
             // Itera a través de los datos de los Pokémon en el JSON
             data.data.forEach((pokemonData, index) => {
                 // Crea el contenido HTML con el enlace para ver más detalles
@@ -19,15 +16,11 @@ document.addEventListener("DOMContentLoaded", function () {
                     <div class="pokemon-card">
                         <h2>${pokemonData.name}</h2>
                         <img src="${pokemonData.images.large}" alt="${pokemonData.name} Image">
-
                         <div class="ver-button-container">
     <a href="detalle-pokemon.html?id=${index}" class="ver-button">Ver más</a>
 </div>
-
-
                     </div>
                 `;
-
                 // Agrega el contenido HTML al contenedor de Pokémon
                 pokemonContainer.innerHTML += htmlContent;
             });
