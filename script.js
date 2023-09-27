ca5200db568b588e17d28d3f12cf8eaeb9f8c24e
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-09-27 13:11:21 -0300

    ...

--- script.js
+++ script.js
@@ -29,3 +29,15 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error("Error al cargar el JSON:", error);
         });
 });
+        // Obtener referencias a elementos HTML
+        var audioPlayer = document.getElementById("audio-player");
+        var ramoncito = document.getElementById("ramoncito");
+        // Agregar un controlador de eventos para el clic en el elemento <span>
+        ramoncito.addEventListener("click", function () {
+            // Si el audio está pausado, reproducirlo; de lo contrario, pausarlo
+            if (audioPlayer.paused) {
+                audioPlayer.play();
+            } else {
+                audioPlayer.pause();
+            }
+        });
