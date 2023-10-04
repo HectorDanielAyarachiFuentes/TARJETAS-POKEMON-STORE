074ef07be03c2023ff9aa94d0ca920d7dbef9ec5
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-10-04 13:29:44 -0300

    ..

--- README.md
+++ README.md
@@ -1,23 +1,12 @@
 # TARJETAS-POKEMON-STORE
-
 ¡Bienvenido a nuestra tienda Pokémon! Aquí encontrarás una amplia colección de 249 tarjetas Pokémon.
-
 ## Visión General de la Tienda
-
 ![Imagen de la tienda Pokémon](https://github.com/HectorDanielAyarachiFuentes/TARJETAS-POKEMON-STORE/blob/main/Fotos%20.md/Opera%20Instant%C3%A1nea_2023-09-30_081605_hectordanielayarachifuentes.github.io.png?raw=true)
-
 En esta vista general, puedes apreciar la amplia selección de tarjetas Pokémon disponibles. Ten en cuenta que debido a la gran cantidad de imágenes, la carga puede tomar un tiempo.
-
 ## Una Visión Más Cercana
-
 ![Tarjetas Pokémon de cerca](https://github.com/HectorDanielAyarachiFuentes/TARJETAS-POKEMON-STORE/blob/main/Fotos%20.md/Opera%20Instant%C3%A1nea_2023-09-30_080811_hectordanielayarachifuentes.github.io.png?raw=true)
-
 En esta imagen, puedes acercarte más a las tarjetas Pokémon en nuestra tienda. Cada una de estas tarjetas tiene detalles únicos que las hacen especiales.
-
 ## Detalles de las Tarjetas Pokémon
-
 ![Detalles de las tarjetas Pokémon](https://github.com/HectorDanielAyarachiFuentes/TARJETAS-POKEMON-STORE/blob/main/Fotos%20.md/Opera%20Instant%C3%A1nea_2023-09-30_080828_hectordanielayarachifuentes.github.io.png?raw=true)
-
 Aquí puedes observar los detalles específicos de algunas tarjetas Pokémon. Nuestra tienda se enorgullece de ofrecer tarjetas de alta calidad para los coleccionistas más apasionados.
-
 ¡Disfruta explorando nuestra tienda y descubriendo todas las tarjetas Pokémon disponibles!
--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const jsonURL = "./json/cards-pokemon-1.json";
     // Elemento donde se mostrarán los Pokémon
     const pokemonContainer = document.getElementById("pokemon-container");
-
     // Realiza una solicitud para cargar el JSON
     fetch(jsonURL)
         .then((response) => response.json())
@@ -25,14 +24,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 // Agrega el contenido HTML al contenedor de Pokémon
                 pokemonContainer.innerHTML += htmlContent;
             });
-
             // Agregar un controlador de eventos para el campo de búsqueda
             const searchInput = document.getElementById("searchbar");
-
             function search_pokemon() {
                 let input = searchInput.value.toLowerCase();
                 let x = document.getElementsByClassName("pokemon-card");
-
                 for (let i = 0; i < x.length; i++) {
                     if (!x[i].innerHTML.toLowerCase().includes(input)) {
                         x[i].style.display = "none";
@@ -41,13 +37,11 @@ document.addEventListener("DOMContentLoaded", function () {
                     }
                 }
             }
-
             searchInput.addEventListener("keyup", search_pokemon);
         })
         .catch((error) => {
             console.error("Error al cargar el JSON:", error);
         });
-
     // Obtener referencias a elementos HTML
     var audioPlayer = document.getElementById("audio-player");
     var ramoncito = document.getElementById("ramoncito");
