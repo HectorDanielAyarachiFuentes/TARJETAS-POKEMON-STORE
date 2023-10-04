fdb0dbcf71878bda99bdc5ca97d819362d551624
Ayarachi Fuentes Hector Daniel <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-10-03 23:12:00 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const jsonURL = "./json/cards-pokemon-1.json";
     // Elemento donde se mostrarán los Pokémon
     const pokemonContainer = document.getElementById("pokemon-container");
+
     // Realiza una solicitud para cargar el JSON
     fetch(jsonURL)
         .then((response) => response.json())
@@ -17,27 +18,46 @@ document.addEventListener("DOMContentLoaded", function () {
                         <h2>${pokemonData.name}</h2>
                         <img src="${pokemonData.images.large}" alt="${pokemonData.name} Image">
                         <div class="ver-button-container">
-    <a href="detalle-pokemon.html?id=${index}" class="ver-button">Ver más</a>
-</div>
+                            <a href="detalle-pokemon.html?id=${index}" class="ver-button">Ver más</a>
+                        </div>
                     </div>
                 `;
                 // Agrega el contenido HTML al contenedor de Pokémon
                 pokemonContainer.innerHTML += htmlContent;
             });
+
+            // Agregar un controlador de eventos para el campo de búsqueda
+            const searchInput = document.getElementById("searchbar");
+
+            function search_pokemon() {
+                let input = searchInput.value.toLowerCase();
+                let x = document.getElementsByClassName("pokemon-card");
+
+                for (let i = 0; i < x.length; i++) {
+                    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+                        x[i].style.display = "none";
+                    } else {
+                        x[i].style.display = "block";
+                    }
+                }
+            }
+
+            searchInput.addEventListener("keyup", search_pokemon);
         })
         .catch((error) => {
             console.error("Error al cargar el JSON:", error);
         });
+
+    // Obtener referencias a elementos HTML
+    var audioPlayer = document.getElementById("audio-player");
+    var ramoncito = document.getElementById("ramoncito");
+    // Agregar un controlador de eventos para el clic en el elemento <span>
+    ramoncito.addEventListener("click", function () {
+        // Si el audio está pausado, reproducirlo; de lo contrario, pausarlo
+        if (audioPlayer.paused) {
+            audioPlayer.play();
+        } else {
+            audioPlayer.pause();
+        }
+    });
 });
-        // Obtener referencias a elementos HTML
-        var audioPlayer = document.getElementById("audio-player");
-        var ramoncito = document.getElementById("ramoncito");
-        // Agregar un controlador de eventos para el clic en el elemento <span>
-        ramoncito.addEventListener("click", function () {
-            // Si el audio está pausado, reproducirlo; de lo contrario, pausarlo
-            if (audioPlayer.paused) {
-                audioPlayer.play();
-            } else {
-                audioPlayer.pause();
-            }
-        });
